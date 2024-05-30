594ff7958703f15c045fc148d60063ac57547807
Anisul Islam <anis@techchaps.com>
2024-05-30 22:23:12 +0500

    addes new screen for summary

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import PaymentSuccessScreen from "./src/screens/payment.successfull";
 import Address from './src/screens/address';
 import ItemsScreen from "./src/screens/items";
 import Summary2 from "./src/screens/summary2";
+import Summary from "./src/screens/summary";
 const App = ()=>{
     return(
         <View>
@@ -13,7 +14,8 @@ const App = ()=>{
             {/* <PaymentSuccessScreen/> */}
             {/* <Address/> */}
             {/* <ItemsScreen/> */}
-            <Summary2/>
+            {/* <Summary2/> */}
+            <Summary/>
         </View>
     )
 }
